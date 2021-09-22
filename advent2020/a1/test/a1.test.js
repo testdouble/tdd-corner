2e95d28634cc1a121671403c55f34f847ac85d01
stevenjackson2 <steve@testdouble.com>
2021-09-22 17:32:09 +0000

    Session 2
    Soooooo close
    Seeing something after we parse the file, expect to be reading a bunch of ints but not getting those back out of

--- advent2020/a1/test/a1.test.js
+++ advent2020/a1/test/a1.test.js
@@ -1,26 +1,45 @@
 const assert = require('assert')
+const fs = require('fs')
 
 function doesMatch2020(i1, i2) {
   return i1 + i2 === 2020
 }
 
 function repair(expenses) {
-  if (expenses.length > 2) return 2019
-  return expenses[0] * expenses[1];
+  let pairs = pairUp(expenses)
+  let matchedPair = pairs.find(pair => doesMatch2020(...pair))
+  return matchedPair[0] * matchedPair[1]
 }
 
 function pairUp(inputs) {
   let index1 = 0
   let index2 = 1
+  let pairs = [];
   for (index1 = 0; index1 < inputs.length -1; index1++) {
     for (index2 = index1 +1; index2 < inputs.length; index2++) {
-      // TODO write code
+      pairs.push([inputs[index1], inputs[index2]])
     }
   }
-  return [[1,2]]
+  return pairs
+}
+
+function readFile(fileName) {
+  return fs.readFileSync(fileName).toString().split("\n").map(parseInt);
+}
+
+function repairFile(fileName) {
+  const expenses = readFile(fileName)
+  console.log(expenses)
+  return repair(expenses)
 }
 
 module.exports = {
+  itReadsAFileAndReturnsAnArrayOfInts: () => {
+    assert.strictEqual(1895, readFile("input.txt")[0])
+  },
+  itReadsAFileAndRepairsTheReport: () => {
+    assert.equal(42, repairFile('input.txt'))
+  },
   itRepairsAZeroExpenseReport: () => {
     assert.equal(0, repair([0, 2020]))
   },
@@ -29,6 +48,12 @@ module.exports = {
   },
   itRepairsAnExpenseReportWithMoreThanTwoValues: () => {
     assert.equal(2019, repair([0, 1, 2019]))
+  },
+  itRepairsAnExpenseReportWithMoreThanTwoValues10s: () => {
+    assert.equal(1020100 , repair([1010, 1, 1010, 0, 2]))
+  },
+  itRepairsDemoExpenseReport: () => {
+    assert.equal(514579, repair([1721,979,366,299,675,1456]))
   },
   itRunsFinds2020: () => {
     assert.equal(true, doesMatch2020(2020, 0))
@@ -43,8 +68,8 @@ module.exports = {
   },
   itReturnsListOfThreePairs:() => {
     const inputs = [1,2,3];
-    const expectedPairs = [[1,2],[2,3],[1,3]]
-    assert.deepEqual(expectedPairs, pairUp(inputs))
+    const expectedPairs = [[1,2],[1,3],[2,3]];
+    assert.deepEqual(expectedPairs, pairUp(inputs));
   },
 }
 
