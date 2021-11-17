4e8f38a32e41cc875b14d6d50b2ce2efb37d1626
Steve Jackson <steve@testdouble.com>
2021-11-17 12:29:59 -0600

    TODO-React Day 3
    "Draw the owl"
    
    Added an input and a submit to the App and trying to get the todo added
    to the list!
    
    Co-authored-by: Andrew Walker <woxorz@gmail.com>
    Co-authored-by: Kenneth Bogner <kenneth.bogner@testdouble.com>
    Co-authored-by: Camilo Payan <camilopayan@users.noreply.github.com>
    Co-authored-by: Jason Grosz <grosz@testdouble.com>
    Co-authored-by: Ed Toro <ed@testdouble.com>
    Co-authored-by: Landon Gray <landon.gray@testdouble.com>

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./todo";
 
 export default () => {
-  return <Todo />;
+  const todoItems = []
+  const [newTodo, setNewTodo] = useState("");
+
+  return (
+    <>
+      <input data-testid="new_todo_input" id="new_todo_input" onChange={(e) => { setNewTodo(e.target.value) } } />
+      <input type="submit" data-testid="new_todo_submit" onClick={() => { todoItems.push(newTodo) } } />
+      <Todo items={ todoItems } />
+    </>
+  );
 };
--- todo-react/src/App.test.jsx
+++ todo-react/src/App.test.jsx
@@ -4,6 +4,7 @@
 import React from "react";
 
 import {
+  fireEvent,
   render,
 } from '@testing-library/react'
 
@@ -19,4 +20,21 @@ it('will render a place to add more todos', () => {
   const output = render(<App />);
   const input = output.queryByTestId("new_todo_input")
   expect(input).toBeTruthy()
+})
+
+it('will render a submit button', () => {
+  const output = render(<App />);
+  const submit = output.queryByTestId("new_todo_submit")
+  expect(submit).toBeTruthy()
+})
+
+it('takes submitted input and adds a todo item', () => {
+  const app = render(<App />);
+  const input = app.queryByTestId("new_todo_input")
+  fireEvent.change(input, {target: { value: "Finish This Test"} });
+  app.queryByTestId("new_todo_submit").click()
+
+  const list = app.queryByTestId("todos")
+  expect(list.children.length).toEqual(1)
+  //expect(list.children[0].textContent).toEqual('Finish This Test');
 })
