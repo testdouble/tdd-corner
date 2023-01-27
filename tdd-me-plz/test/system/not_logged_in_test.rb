848ebe14eb214e07dffd47f552ddb7dcf520df0f
Josh Justice <me@codingitwrong.com>
2023-01-27 14:16:59 -0500

    Record our next intended test as a failing test

--- tdd-me-plz/test/system/not_logged_in_test.rb
+++ tdd-me-plz/test/system/not_logged_in_test.rb
@@ -13,6 +13,14 @@ class NotLoggedInTest < ApplicationSystemTestCase
         assert_text "You are not allowed to make a new proposal."
     end
 
+    test 'that it can display a different auth error message, not just a hardcoded one' do
+        flunk 'figure out if or how to test this'
+
+        # Brainstorm from Josh: why do we care? We don't care until we actually have a
+        # need to display a different message. If we know that case, we can test it.
+        # Otherwise, maybe move on.
+    end
+
     # test 'can access new proposal page when logged in' do
     #     # log in
 
